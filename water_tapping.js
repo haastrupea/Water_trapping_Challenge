ea98a1f2687db0f86519ace2d1e2a2a21e373991
Haastrup Elijah <haastrupea@gmail.com>
2021-11-12 10:11:17 +0100

    chore: complete visualization

--- water_tapping.js
+++ water_tapping.js
@@ -45,27 +45,24 @@ function trapping (height) {
 function trappingV2 (height = []) {
   let trappedWater = 0
   height.forEach((elm, i,) => {
-
-    // split heights array into right and left array using the current index
-    const leftArray = height.slice(0,i+1) // create a left array with current height included
-    let tallLeft = Math.max(...leftArray) // find the max
-
-    const rightArray = height.slice(i) // create a right array with current height included
-    let tallRight = Math.max(...rightArray) // find the max
-
-
-    const minHeight = Math.min(tallRight, tallLeft)
-
-    trappedWater = trappedWater + (minHeight - elm)
-    
+    trappedWater = trappedWater + waterTrappedPerHeight(height, i,elm)
   })
 
   return trappedWater;
 };
 
 
-const data = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-const data2 = [4,2,0,3,2,5]
+function waterTrappedPerHeight (height, i,elm) {
 
-console.log(data.toString(), 'will trap',trappingV2(data),'Unit(s) of water')
-console.log(data2.toString(), 'will trap',trappingV2(data2),'Unit(s) of water')
+      // split heights array into right and left array using the current index
+      const leftArray = height.slice(0,i+1) // create a left array with current height included
+      let tallLeft = Math.max(...leftArray) // find the max
+  
+      const rightArray = height.slice(i) // create a right array with current height included
+      let tallRight = Math.max(...rightArray) // find the max
+  
+  
+      const minHeight = Math.min(tallRight, tallLeft)
+
+      return (minHeight - elm)
+}
